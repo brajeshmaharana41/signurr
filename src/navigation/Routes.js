aab04bf42c8d60023b5aebfd751f5ba3306e103b
Brajesh Maharana <maharana.brajesh@gmail.com>
2021-07-14 18:42:45 +0530

    commit

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -9,8 +9,16 @@ import SplashScreen from '../screens/public/SplashScreen'
 import SplashScreen1 from '../screens/public/startingScreens/SplashScreen1'
 import SplashScreen2 from '../screens/public/startingScreens/SplashScreen2'
 import SplashScreen3 from '../screens/public/startingScreens/SplashScreen3'
+//Auth Screen
 import LoginScreen from '../screens/auth/LoginScreen'
+import SignupScreen from '../screens/auth/SignupScreen'
+//screens
+import AboutScreen from '../screens/public/AboutScreen'
+import ContactScreen from '../screens/public/ContactScreen'
+import HomeScreen from '../screens/public/HomeScreen'
+import SettingsScreen from '../screens/public/SettingsScreen'
 import { COLORS } from '../theme/Theme'
+//import { Tab } from 'react-native-elements/dist/tab/Tab'
 
 // stacks
 const SplashStack = createStackNavigator()
@@ -20,6 +28,31 @@ const Tabs = createBottomTabNavigator();
 const MainStack = createStackNavigator()
 const Drawer = createDrawerNavigator()
 
+const TabsNavigator = ({navigation}) => (
+    <Tabs.Navigator>
+        <Tabs.Screen name="Home" component={HomeScreen} />
+        <Tabs.Screen name="About" component={AboutScreen} />
+        <Tabs.Screen name="Contact" component={ContactScreen} />
+        <Tabs.Screen name="Setting" component={SettingsScreen} />
+    </Tabs.Navigator>
+)
+const DrawerNavigator = ({ navigation }) => (
+    <Drawer.Navigator 
+        initialRouteName="Home" 
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+        drawerContentOptions={{
+            activeTintColor: COLORS.white,
+            activeBackgroundColor: COLORS.primary,
+            inactiveTintColor: COLORS.dark,
+            inactiveBackgroundColor: COLORS.white
+        }}
+    >
+        <Drawer.Screen name="Home" component={TabsNavigator} />
+        <Drawer.Screen name="About" component={AboutScreen} />
+        <Drawer.Screen name="Contact" component={ContactScreen} />
+        <Drawer.Screen name="Login" component={LoginScreen} />
+    </Drawer.Navigator>
+)
 const SplashStackNavigator = ({ navigation }) => (
     <SplashStack.Navigator screenOptions={{ headerShown: false }} >
         <SplashStack.Screen name="Splash" component={SplashScreen} />
@@ -30,38 +63,83 @@ const AuthStackNavigator = ({ navigation }) => (
     <AuthStack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Splash1">
         {/* <AuthStack.Screen name="PermissonsPOC" component={PermissonsPOC} /> */}
         {/* <AuthStack.Screen name="Permissions" component={PermissionsScreen} /> */}
-        {/* <AuthStack.Screen name="Home" component={HomeScreen} /> */}
+        <AuthStack.Screen name="Home" component={DrawerNavigator} />
         <AuthStack.Screen name="Login" component={LoginScreen} />
         <AuthStack.Screen name="Splash1" component={SplashScreen1} />
         <AuthStack.Screen name="Splash2" component={SplashScreen2} />
         <AuthStack.Screen name="Splash3" component={SplashScreen3} />
-        {/* <AuthStack.Screen name="Signup" component={SignupScreen} />
-        <AuthStack.Screen name="ForgotPassword" component={ForgotPasswordScreen} /> */}
+        <AuthStack.Screen name="Signup" component={SignupScreen} />
+        {/*<AuthStack.Screen name="ForgotPassword" component={ForgotPasswordScreen} /> */}
     </AuthStack.Navigator>
 )
-// const DrawerNavigator = ({ navigation }) => (
-//     <Drawer.Navigator 
-//         initialRouteName="Home" 
-//         drawerContent={(props) => <CustomDrawerContent {...props} />}
-//         drawerContentOptions={{
-//             activeTintColor: COLORS.white,
-//             activeBackgroundColor: COLORS.primary,
-//             inactiveTintColor: COLORS.dark,
-//             inactiveBackgroundColor: COLORS.white
-//         }}
-//     >
-//         <Drawer.Screen name="Home" component={HomeScreen} />
-//         <Drawer.Screen name="About" component={AboutScreen} />
-//         <Drawer.Screen name="Videos" component={VideosScreen} />
-//         <Drawer.Screen name="Departments" component={DepartmentsScreen} />
-//         <Drawer.Screen name="Donations" component={DonationsScreen} />
-//         <Drawer.Screen name="News" component={NewsScreen} />
-//         <Drawer.Screen name="Contacts" component={ContactsScreen} />
-//         <Drawer.Screen name="Shop" component={ShopScreen} />
-//         <Drawer.Screen name="Login" component={LoginScreen} />
-//         <Drawer.Screen name="Dashboard" component={DashboardScreen} />
-//     </Drawer.Navigator>
-// )
+const HomeStackNavigator = ({ navigation }) => (
+    <HomeStack.Navigator
+        screenOptions={{
+            headerShown: true,
+            headerStyle: {
+                backgroundColor: COLORS.primary
+            },
+            headerTitle: props => (
+                <Image
+                    style={{ left: -30, height: 50 }}
+                    source={require('../assets/logo.png')}
+                    resizeMode='contain'
+                />
+            ),
+            headerLeft: () => (
+                <TouchableOpacity
+                    onPress={() => navigation.openDrawer()}
+                >
+                    <Ionicon name="menu" size={25} color={COLORS.white} />
+                </TouchableOpacity>
+            ),
+            headerLeftContainerStyle: {
+                paddingLeft: 15
+            }
+        }}
+        initialRouteName="Home"
+    >
+        <HomeStack.Screen name="Home" component={HomeScreen} />
+        <HomeStack.Screen name="About" component={AboutScreen} />
+        <HomeStack.Screen name="Videos" component={VideosScreen} />
+        <HomeStack.Screen name="Departments" component={DepartmentsScreen} />
+        <HomeStack.Screen name="Donations" component={DonationsScreen} />
+        <HomeStack.Screen name="News" component={NewsScreen} />
+        <HomeStack.Screen name="Contacts" component={ContactsScreen} />
+        <HomeStack.Screen name="Shop" component={ShopScreen} />
+        <HomeStack.Screen name="Login" component={LoginScreen} />
+    </HomeStack.Navigator>
+)
+
+function CustomDrawerContent(props) {
+    const { navigation } = props
+    return (
+        <DrawerContentScrollView {...props} contentContainerStyle={{ flex: 1, justifyContent: 'space-between' }}>
+            <View>
+                <DrawerItemList {...props} />
+                {/* <Text style={{color: theme.colors.text}}>User</Text> */}
+            </View>
+            {/* <Button backgroundColor={theme.colors.background} style={{ backgroundColor: theme.colors.background }}
+                title="Toggle App Theme" onPress={() => toggleTheme()}
+            // onPress={() => { navigation.navigate('Products') }}
+            /> */}
+            {/* <TouchableOpacity onPress={() => toggleTheme(navigation)}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 15, paddingVertical: 10 }}>
+                    <Text style={{ color: theme.colors.text }}>{isDarkTheme ? 'Light' : 'Dark'} Theme</Text>
+                    <View pointerEvents="none">
+                        <Switch
+                            trackColor={{ false: "#767577", true: "#721602" }}
+                            thumbColor={isDarkTheme ? theme.colors.primary : "#f4f3f4"}
+                            ios_backgroundColor={isDarkTheme ? theme.colors.primary : "#3e3e3e"}
+                            value={isDarkTheme}
+                        />
+                    </View>
+                </View>
+            </TouchableOpacity> */}
+        </DrawerContentScrollView>
+    )
+}
+
 
 const Routes = () => {
      const [loading, setLoading] = useState(true)
@@ -75,7 +153,7 @@ const Routes = () => {
 
     return (
         <NavigationContainer>
-             {loading ? <SplashStackNavigator /> : <AuthStackNavigator />}
+             {loading ? <SplashStackNavigator /> : <AuthStackNavigator /> }
         </NavigationContainer>
     )
 }
